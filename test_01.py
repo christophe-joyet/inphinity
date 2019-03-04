3a604e627c97993e4799c873c62c01937f736b0d
Christophe <christophejoyet@gmail.com>
2019-03-04 16:05:52 +0100

    Mise au propre du code de test_01.py

--- test_01.py
+++ test_01.py
@@ -42,7 +42,10 @@ def getAllSourceData():
     return list_Source_data
 
 # Make figure and axes
-list_bacterium = getAllBacterium()
+#=============================================================
+#chart - Number of phages and Number of Bacterium total in DB Inphinity
+
+'''list_bacterium = getAllBacterium()
 list_bacteriophage = getAllBacteriophages()
 data = [len(list_bacteriophage), len(list_bacterium)]
 data_name = ['Phages', 'Bacteries']
@@ -52,6 +55,12 @@ fig1, ax1 = plt.subplots()
 ax1.pie(data, labels=data_name, autopct='%1.1f%%', startangle=90)
 plt.show()
 '''
+
+fig, axs = plt.subplots(3, 2)
+
+#=============================================================
+#chart how many differents phages and how many differents bacteries
+
 list_bacterium = []
 list_bacteriophage = []
 
@@ -68,49 +77,18 @@ for couple in list_couple:
 data = [len(list_bacteriophage), len(list_bacterium)]
 data_name = ['Phages', 'Bacteries']
 
-#plot pie
-fig1, ax1 = plt.subplots()
-ax1.pie(data, labels=data_name, startangle=90)
-plt.show()
-'''
-'''
-fig, axs = plt.subplots(3, 2)
-
-# Create chart to compare number of phages in the database
-# and the number of phages who have strain interaction with Bacterium
-# bacteriophages = getAllBacteriophages()
-#
-#=============================================================
-
-#chart to compare types of the interactions between couples
-labels = ['type 0', 'type 1', 'type 2', 'type 3', 'type 4']
-phages_with_interaction_type = [0] * len(labels)
-list_couple = getAllCouples()
-
-for couple in list_couple:
-    phages_with_interaction_type[couple.interaction_type] += 1
-
-#delete null value
-i = 0
-while(i < len(labels)):
-  if(phages_with_interaction_type[i] == 0):
-    del phages_with_interaction_type[i]
-    del labels[i]
-    continue
-  i += 1
-
-#plot pie
-axs[0,0].set_title("Type of the interaction for all the couples")
-wedges, texts, autotexts = axs[0,0].pie(phages_with_interaction_type, autopct='%.0f%%')
-axs[0,0].legend(wedges, labels,
-          title="Type",
+axs[0,0].set_title("Bacteries / Phages in couples")
+wedges, texts, autotexts = axs[0,0].pie(data, autopct='%.0f%%')
+axs[0,0].legend(wedges, data_name,
+          title="Bacteries / Phages in couples",
           loc="center left",
           bbox_to_anchor=(1, 0, 0.5, 1))
 
 #=============================================================
-
 #chart to know who has insterted couples
+
 list_person = getAllPersonResponsible()
+list_couple = getAllCouples()
 list_person_name = []
 
 for person in list_person:
@@ -144,7 +122,6 @@ axs[0,1].legend(wedges, list_person_name,
 
 #=============================================================
 #chart source data
-
 list_source_data = getAllSourceData()
 list_source_data_id = []
 list_source_data_name = []
@@ -196,7 +173,6 @@ for bact in list_couple:
 for specie in list_species:
   list_species_name.append(specie.designation)
 
-
 #dictionnary {bact_id : number of bact}
 dic = {bact_id: 0 for bact_id in list_bact_id}
 
@@ -212,7 +188,6 @@ for value in dic.values():
 axs[1,1].set_title("Species of bacterium")
 axs[1,1].pie(list_value)
 
-#plt.show()
 #=============================================================
 #chart to know level interaction
 taxonomy = ['Family', 'Gender', 'Species', 'Strain']
@@ -236,7 +211,6 @@ axs[2,0].legend(wedges, taxonomy,
           title="Taxonomy",
           loc="center left",
           bbox_to_anchor=(1, 0, 0.5, 1))
-#plt.show()
 
 #=============================================================
 #validity of each couple
@@ -244,12 +218,10 @@ axs[2,0].legend(wedges, taxonomy,
 validity = []
 number_bact_validity = []
 for couple in list_couple:
-  if couple.validity in validity:
-    number_bact_validity[validity.index(couple.validity)] += 1
-  else:
+  if not couple.validity in validity:
     validity.append(couple.validity)
     number_bact_validity.append(0)
-    number_bact_validity[validity.index(couple.validity)] += 1
+  number_bact_validity[validity.index(couple.validity)] += 1
 
 axs[2,1].set_title("Validity of Each Couple")
 wedges, texts, autotexts = axs[2,1].pie(number_bact_validity, autopct='%.0f%%')
@@ -258,5 +230,5 @@ axs[2,1].legend(wedges, validity,
           loc="center left",
           bbox_to_anchor=(1, 0, 0.5, 1))
 
-#plt.show()'''
+plt.show()
 
