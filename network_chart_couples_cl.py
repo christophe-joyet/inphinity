621a70f75e67e44bdf0007e45538832035d71615
Christophe <christophejoyet@gmail.com>
2019-03-24 18:38:00 +0100

    ajout des designations des phages et des bactéries

--- network_chart_couples_cl.py
+++ network_chart_couples_cl.py
@@ -12,6 +12,9 @@ from objects_API.BacteriumJ import BacteriumJson
 from objects_API.OrganismJ import OrganismJson
 from objects_API.BacteriophageJ import BacteriophageJson
 
+from Bio import Entrez #to import data from GenBank
+Entrez.email = "christophe.joyet@heig-vd.ch"
+
 conf_obj = ConfigurationAPI()
 conf_obj.load_data_from_ini()
 AuthenticationAPI().createAutenthicationToken()
@@ -32,10 +35,10 @@ SEMI_CLEAR_LYSIS_1E7MINUS = 11
 ALL_CLEAR_LYSIS = [CLEAR_LYSIS, CLEAR_LYSIS_1E7PLUS, CLEAR_LYSIS_1E7MINUS]
 ALL_SEMI_CLEAR_LYSIS = [SEMI_CLEAR_LYSIS, SEMI_CLEAR_LYSIS_1E7PLUS, SEMI_CLEAR_LYSIS_1E7MINUS]
 
-#==================================IMPLEMENTATION FUNCTIONS BEGIN===============================
+#==================================IMPLEMENTATION OF FUNCTIONS===============================
 
 #Draw network graph to show relations between phages and bacterium
-def draw_graph(phages, bacterium, lysis_type,
+def draw_graph(phages:list, bacterium:list, list_couples_lysis_type:list,
                node_size=1600, node_alpha=0.5,
                node_text_size=12,
                edge_alpha=0.6, edge_tickness=0.5,
@@ -52,10 +55,14 @@ def draw_graph(phages, bacterium, lysis_type,
     nodes_bacterium = []
     
     for couple in list_couples_lysis_type:
-        if not couple.bacterium in nodes_bacterium:
-            nodes_bacterium.append(couple.bacterium)
-        if not couple.bacteriophage in nodes_phages:
-            nodes_phages.append(couple.bacteriophage)
+        couple_bacterium = BacteriumJson.getByID(couple.bacterium).acc_number
+        if not couple_bacterium in nodes_bacterium:
+            nodes_bacterium.append(couple_bacterium)
+        
+        #get phages' designation
+        couple_bacteriophage = BacteriophageJson.getByID(couple.bacteriophage).designation
+        if not couple_bacteriophage in nodes_phages:
+            nodes_phages.append(couple_bacteriophage)
 
     print("Nombre de phages différents : " + str(len(nodes_phages)))
     print("Nombre de bactéries différentes : " + str(len(nodes_bacterium)))
@@ -101,14 +108,14 @@ def draw_graph(phages, bacterium, lysis_type,
     ax.set_xlabel('Green = Bacterium\n Red = Phages')
     #plt.show()
     #save graph in png
-    plt.savefig('network_CL.png')
+    plt.savefig('network_CL_E7P_E7M.png')
 
 #get all couples of the DB inphinity
 def getAllOfCouples():
     list_couples = CoupleJson.getAllAPI()
     return list_couples
 
-#get couples in function of lysis classification
+#get couples according to their lysis classification
 def getCouplesLysis(lysis_type):
     
     #verify if lysis is an int 
@@ -128,7 +135,46 @@ def getCouplesLysis(lysis_type):
 
     return list_couples_lysis_type
 
-#==================================IMPLEMENTATION FUNCTIONS END===============================
+#get couples dictionnary according to their taxonomie
+def getCouplesTaxonomie(couples_list:list):
+    #Level interaction
+    STRAIN = 1
+    SPECIES = 2
+    GENUS = 3
+    FAMILY = 4
+
+    taxonomie_dictionnary = {STRAIN:0, SPECIES:0, GENUS:0, FAMILY:0}
+    for couples in couples_list:
+        if couples.level in taxonomie_dictionnary:
+            taxonomie_dictionnary[couples.level] += 1
+
+    return taxonomie_dictionnary
+
+#get accession number
+def getAccessionNumber(organism_list:list):
+    accessions_number_list = []
+    for organism in organism_list:
+        accessions_number_list.append(organism.acc_number)
+    
+    return accessions_number_list
+
+#get definition of an organism using Genbank
+'''def getOrganismDefinitionFromGenBank(organism_list:list):
+    organism_definition_list = []
+    i = 0
+
+    while(i < len(organism_list)):
+        query = str(organism_list[i])
+        handle = Entrez.esearch(db='nucleotide', term=query)
+        record = Entrez.read(handle)
+        gi = record['IdList']
+        protein_info = Entrez.efetch(db="nucleotide",id=gi,rettype="gb",retmode="xml")
+        protein_info = Entrez.read(protein_info)
+
+        organism_definition_list.append()
+
+        i += 1'''
+#==================================IMPLEMENTATION OF FUNCTIONS END===============================
 
 #choose what type of lysis we want
 lysis_type = CLEAR_LYSIS
@@ -142,16 +188,19 @@ print("Nombre de couples clear lysis : " + str(len(list_couples_lysis_type)))
 phages = []
 bacterium = []
 
-#for couple in list_couples_lysis_type:
- #   phages.append(BacteriophageJson.getByID(couple.bacteriophage))
- #   bacterium.append(BacteriumJson.getByID(couple.bacterium))
-
 for couple in list_couples_lysis_type:
+    phages.append(BacteriophageJson.getByID(couple.bacteriophage).designation)
+    bacterium.append(BacteriumJson.getByID(couple.bacterium).acc_number)
+
+'''for couple in list_couples_lysis_type:
     phages.append(couple.bacteriophage)
-    bacterium.append(couple.bacterium)
+    bacterium.append(couple.bacterium)'''
 
-import networkx as nx
-import matplotlib.pyplot as plt
+print("phage" + str(len(phages)))
+print("bacterium" + str(len(bacterium)))
 
+couples_tax = getCouplesTaxonomie(list_couples_lysis_type)
+print(couples_tax)
+print(len(list_couples_lysis_type))
 # network graph
-draw_graph(phages, bacterium, lysis_type)
+draw_graph(phages, bacterium, list_couples_lysis_type)
